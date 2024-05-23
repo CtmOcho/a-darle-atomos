d3b28d0f691565066cbc765dcfff68ebab6fc586
Benjamín Esteban Soto Alfán <105157002+FoxOfTheVoid@users.noreply.github.com>
2024-05-22 20:00:57 -0400

    Update Balanza.cs

--- A darle atomos/Assets/Code/Balanza.cs
+++ A darle atomos/Assets/Code/Balanza.cs
@@ -8,6 +8,8 @@ public class Balanza : MonoBehaviour
     float weight = 0.000f;
 
     public TextMeshPro scaleTMP;
+    private List<Rigidbody> objectsOnScale = new List<Rigidbody>();
+
 
     // Start is called before the first frame update
     void Start()
@@ -20,13 +22,34 @@ public class Balanza : MonoBehaviour
     {
         scaleTMP.text = weight.ToString();
     }
+
     private void OnTriggerEnter(Collider other)
     {
-        weight += Mathf.Abs(other.GetComponent<Rigidbody>().mass*1000);
-        
+    
+        Rigidbody otherRigidbody = other.GetComponent<Rigidbody>();
+        if (otherRigidbody != null && !objectsOnScale.Contains(otherRigidbody))
+        {
+            
+            objectsOnScale.Add(otherRigidbody);
+            weight += Mathf.Abs(otherRigidbody.mass * 1000);
+        }
     }
+
     private void OnTriggerExit(Collider other)
     {
-        weight -= Mathf.Abs(other.GetComponent<Rigidbody>().mass*1000);
+        Rigidbody otherRigidbody = other.GetComponent<Rigidbody>();
+        if (otherRigidbody != null && objectsOnScale.Contains(otherRigidbody))
+        {
+
+            objectsOnScale.Remove(otherRigidbody);
+
+            weight -= Mathf.Abs(otherRigidbody.mass * 1000);
+
+
+            if (weight < 0)
+            {
+                weight = 0;
+            }
+        }
     }
 }
