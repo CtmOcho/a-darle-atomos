76fb990db4f0e968e9c0a2c9bb6b9d90f60b042a
Leandro Espinoza <leandro.espinoza@usm.cl>
2024-06-02 17:51:21 -0400

    Migrated to HD Rendering Pipeline, imported VFX Graph and added fire demo scene

---
+++ A darle atomos/Assets/Scripts/fire_controller.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using JetBrains.Annotations;
+using UnityEngine;
+using UnityEngine.VFX;
+
+public class fire_controller : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
