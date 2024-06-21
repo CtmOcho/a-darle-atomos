53fceacf10b7a1bf39afbb517a08a280223ee9ef
Leandro Espinoza <leandro.espinoza@usm.cl>
2024-06-21 15:57:23 -0400

    fix previous commit

--- A darle atomos/Assets/Scripts/FireReagentController.cs
+++ A darle atomos/Assets/Scripts/FireReagentController.cs
@@ -4,20 +4,23 @@ using UnityEngine;
 
 public class FireReagentController : MonoBehaviour
 {
-    [SerializeField]
-    private Color colorLlama;
+    public int colorLlama;
     [SerializeField]
     private bool estaMojado = false;
 
     private void Start()
     {
-        colorLlama = Color.black;
+        colorLlama = 4;
         estaMojado = false;
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        Reagent reactivo = other.GetComponent<ReagentProperties>().getReagent();
+        ReagentProperties reagentProperties = other.GetComponent<ReagentProperties>();
+        if (reagentProperties == null){
+            return;
+        }
+        Reagent reactivo = reagentProperties.getReagent();
         if (reactivo != null)
         {
             if (!reactivo.IsSolid)
