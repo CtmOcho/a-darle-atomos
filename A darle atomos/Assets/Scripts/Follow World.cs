75681714d29419872ede86b389fa0fe63e8c3314
Martin <martin.sarubbi@usm.cl>
2024-10-05 19:52:29 -0300

    Se agregan iconos (flecha y warning) y script para poner iconos arriba de objetos

---
+++ A darle atomos/Assets/Scripts/Follow World.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowWorld : MonoBehaviour
+{
+
+    [Header("Tweaks")]
+    [SerializeField] public Transform lookAt;
+    [SerializeField] public Vector3 offset;
+
+    [Header("Logic")]
+    private Camera cam;
+    void Start()
+    {
+        //cam = Camera.main;
+    }
+
+    void Update()
+    {
+        Vector3 pos = lookAt.position + offset;
+
+        if(transform.position != pos){
+            transform.position = pos;
+        }
+    }
+}
