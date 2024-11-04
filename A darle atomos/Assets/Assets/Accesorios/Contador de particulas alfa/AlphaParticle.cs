ff36f85e79a91c0926352d989e79b846f267ef95
Fabian Lorca <fabian.lorca22@gmail.com>
2024-11-03 21:51:25 -0300

    Experiencia molecular Rutherford y avance Exp Pasta de Elefante

--- A darle atomos/Assets/Assets/Accesorios/Contador de particulas alfa/AlphaParticle.cs
+++ A darle atomos/Assets/Assets/Accesorios/Contador de particulas alfa/AlphaParticle.cs
@@ -7,25 +7,42 @@ public class AlphaParticle : MonoBehaviour
     public AnimationCurve weight;
     public LayerMask mask;
     Vector3 destinationPos;
+
+    public bool isSubExp = false;
     void Start()
     {
-        UnityEngine.Random.InitState(DateTime.Now.Millisecond);
-        RaycastHit hit;
-        Physics.Raycast(transform.position, transform.forward, out hit, 2f, mask);
-        destinationPos = hit.point;
-        transform.LookAt(destinationPos);
+        
+        if(isSubExp)
+        {
+            GetComponent<Rigidbody>().isKinematic = false;
+            GetComponent<Rigidbody>().useGravity = false;
+            GetComponent<Rigidbody>().AddForce(transform.forward * particleSpeed);
+        }
+        else
+        {
+            UnityEngine.Random.InitState(DateTime.Now.Millisecond);
+            RaycastHit hit;
+            Physics.Raycast(transform.position, transform.forward, out hit, 5f, mask);
+            destinationPos = hit.point;
+            transform.LookAt(destinationPos);
+        }
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position = Vector3.MoveTowards(transform.position, destinationPos, particleSpeed * Time.deltaTime);
-        if(Vector3.Distance(transform.position, destinationPos) < 0.001f)
+        if (!isSubExp)
         {
-            float randAngle = Mathf.Lerp(0, 360, GetWeightedNumber());
-            transform.Rotate(0, -randAngle, 0);
-            destinationPos = destinationPos + transform.forward * 2;
+            transform.position = Vector3.MoveTowards(transform.position, destinationPos, particleSpeed * Time.deltaTime);
+            if (Vector3.Distance(transform.position, destinationPos) < 0.001f)
+            {
+                float randAngle = Mathf.Lerp(0, 360, GetWeightedNumber());
+                transform.Rotate(0, -randAngle, 0);
+                destinationPos = destinationPos + transform.forward * 2;
+            }
         }
+        
     }
     public float GetWeightedNumber()
     {
--- A darle atomos/Assets/Assets/Accesorios/Contador de particulas alfa/AlphaParticleGenerator.cs
+++ A darle atomos/Assets/Assets/Accesorios/Contador de particulas alfa/AlphaParticleGenerator.cs
@@ -1,7 +1,7 @@
 using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
 
 public class AlphaParticleGenerator : MonoBehaviour
 {
@@ -9,18 +9,40 @@ public class AlphaParticleGenerator : MonoBehaviour
     public GameObject alphaParticle;
     public int maxParticles;
     public AnimationCurve weight;
+    [Header("Variables Experiencia Molecular")]
+    public bool isSubExp = false;
+    public GameObject mainCam;
+    public GameObject povCam;
+    public GameObject titulos;
+    public GameObject panel;
+    public TextMeshProUGUI buttonText;
+    [SerializeField]
+    private TextMeshProUGUI explanation;
 
     int subCounter;
+    int swapTimeoutCounter;
     float randAngle;
     float randRadius;
     float radius;
+    bool mainCamFlag;
 
     GameObject[] particleArr;
+    GameObject[] particleCameraArr;
     void Start()
     {
         UnityEngine.Random.InitState(DateTime.Now.Millisecond);
         subCounter = 0;
-        radius = 0.006f;
+        
+        if (isSubExp) 
+        {
+            radius = 0.6f;
+            povCam.GetComponent<Camera>().enabled = false; 
+        }
+        else
+        {
+            radius = 0.006f;
+        }
+        mainCamFlag = false;
     }
 
     // Update is called once per frame
@@ -29,6 +51,7 @@ public class AlphaParticleGenerator : MonoBehaviour
         if(controller.isOn)
         {
             particleArr = GameObject.FindGameObjectsWithTag("alphaParticle");
+            particleCameraArr = GameObject.FindGameObjectsWithTag("particleCam");
             if (particleArr.Length < maxParticles && subCounter > 50)
             {
                 randAngle = UnityEngine.Random.Range(0, 360);
@@ -36,7 +59,15 @@ public class AlphaParticleGenerator : MonoBehaviour
                 Vector3 point = new Vector3(randRadius, 0, 0);
                 Vector3 rotatedPoint = Quaternion.Euler(0, 0, randAngle) * point;
                 GameObject particle = Instantiate(alphaParticle, transform.TransformPoint(rotatedPoint), transform.rotation);
-                Destroy(particle, 2);
+                Destroy(particle, 3);
+                if (isSubExp && particleCameraArr.Length < 1)
+                { 
+                    GameObject povCamInstance = Instantiate(povCam, transform.TransformPoint(rotatedPoint), transform.rotation);
+                    povCamInstance.GetComponent<Camera>().enabled = true;
+                    povCamInstance.tag = "particleCam";
+                    povCamInstance.transform.SetParent(particle.transform, false);
+                    povCamInstance.transform.localPosition = Vector3.zero;
+                }
                 subCounter = 0;
             }
             subCounter++;
@@ -45,5 +76,41 @@ public class AlphaParticleGenerator : MonoBehaviour
     public float GetWeightedNumber()
     {
         return weight.Evaluate(UnityEngine.Random.value);
+    }
+
+    public void SwapView()
+    {
+        //Si mainCamFlag es true, entonces se activa la povCam, de lo contrario, se activa la mainCam
+        if (isSubExp)
+        {
+            mainCamFlag = !mainCamFlag;
+            swapTimeoutCounter = 0;
+            controller.isOn = mainCamFlag;
+            titulos.SetActive(!mainCamFlag);
+            panel.SetActive(mainCamFlag);
+            particleCameraArr = GameObject.FindGameObjectsWithTag("particleCam");
+            while (swapTimeoutCounter < 500)
+            {
+                if (particleCameraArr.Length >= 1)
+                {
+                    particleCameraArr[0].GetComponent<Camera>().enabled = mainCamFlag;
+                    if (!mainCamFlag) Destroy(particleCameraArr[0]); 
+                    mainCam.GetComponent<Camera>().enabled = !mainCamFlag;
+                    break;
+                }
+                swapTimeoutCounter++;
+            }
+            
+            if (!mainCamFlag)
+            {
+                buttonText.text = "INCIAR";
+                explanation.text = "En esta experiencia, observará el experimento de Rutherford desde la perspectiva de una particula alfa.";
+            }
+            else 
+            { 
+                buttonText.text = "DETENER";
+                explanation.text = "Se puede observar que los átomos de oro están compuestos mayoritariamente de vacío, es por esto que algunas partículas rebotan y otras traspasan la lámina de oro.";
+            }
+        }
     }
 }
